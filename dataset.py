199ecde55eb4af8ea3f610205b4c07cefb0804ec
Lê Ngọc Phương Nga <23520992@gm.uit.edu.vn>
2025-07-20 08:49:57 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,60 +1,46 @@
 import os
+import numpy as np
 from PIL import Image
-import torch
 from torch.utils.data import Dataset
-import torchvision.transforms as T
+import torch
 
 class CarDamageDataset(Dataset):
-    def __init__(self, img_dir, mask_dir, names_list,
-                 img_transform=None, mask_transform=None):
-        """
-        img_dir:    folder chứa ảnh (.jpg/.jpeg)
-        mask_dir:   folder chứa mask (.png)
-        names_list: list tên file (không đuôi), vd ["0001", ...]
-        img_transform:  torchvision.transforms áp dụng lên ảnh
-        mask_transform: torchvision.transforms áp dụng lên mask
-        """
-        self.img_dir        = img_dir
-        self.mask_dir       = mask_dir
-        self.img_transform  = img_transform
-        self.mask_transform = mask_transform
+    """
+    Dataset cho segment car damage.
+    - img_dir: thư mục chứa ảnh .jpg
+    - anno_dir: thư mục chứa mask .png (hoặc .bmp) cùng tên file
+    - split_file: đường dẫn tới file .txt liệt kê tên (không có đuôi) các sample
+    - transform: torchvision.transforms áp cho ảnh (không áp mask!)
+    """
+    def __init__(self, img_dir, anno_dir, split_file, transform=None):
+        self.img_dir   = img_dir
+        self.anno_dir  = anno_dir
+        self.transform = transform
 
-        # Lọc những sample có cả ảnh và mask
-        valid = []
-        for name in names_list:
-            # check ảnh
-            found_img = any(os.path.exists(os.path.join(img_dir, name + ext))
-                            for ext in (".jpg", ".jpeg"))
-            # check mask
-            found_mask = os.path.exists(os.path.join(mask_dir, name + ".png"))
-            if found_img and found_mask:
-                valid.append(name)
-        self.names = valid
-        print(f"[Dataset] {len(self.names)}/{len(names_list)} valid samples")
+        # Đọc danh sách tên sample (ví dụ dòng "000123")
+        with open(split_file, 'r') as f:
+            self.names = [line.strip() for line in f if line.strip()]
+
+        # Nếu mask là binary (0/1), num_classes=2; nếu chỉ 1 lớp foreground, num_classes=1
+        self.num_classes = 2
 
     def __len__(self):
         return len(self.names)
 
     def __getitem__(self, idx):
         name = self.names[idx]
+        # load ảnh
+        img_path  = os.path.join(self.img_dir,  name + '.jpg')
+        mask_path = os.path.join(self.anno_dir, name + '.png')
+        img  = Image.open(img_path).convert('RGB')
+        mask = Image.open(mask_path).convert('L')  # grayscale mask
 
-        # 1) Load image
-        for ext in (".jpg", ".jpeg"):
-            img_path = os.path.join(self.img_dir, name + ext)
-            if os.path.exists(img_path):
-                image = Image.open(img_path).convert("RGB")
-                break
+        # áp transform cho ảnh (không áp trực tiếp mask, chỉ resize v.v.)
+        if self.transform:
+            img = self.transform(img)
+            # mask resize phải dùng nearest để giữ nhãn nguyên
+            mask = self.transform(mask).long()
 
-        # 2) Load mask
-        mask_path = os.path.join(self.mask_dir, name + ".png")
-        mask      = Image.open(mask_path).convert("L")  # grayscale
+        # mask: tensor kích thước [1,H,W] với giá trị 0 hoặc 1
+        return img, mask
 
-        # 3) Apply transforms
-        if self.img_transform:
-            image = self.img_transform(image)
-        if self.mask_transform:
-            mask = self.mask_transform(mask)
-            # mask tensor will be float in [0,1], convert to binary 0/1
-            mask = (mask > 0).float()
-
-        return image, mask
