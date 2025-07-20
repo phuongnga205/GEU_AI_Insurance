9a23f9bc76934ac88c96b9b968832402f5f44ac5
Lê Ngọc Phương Nga <23520992@gm.uit.edu.vn>
2025-07-20 09:05:09 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,46 +1,68 @@
 import os
 from PIL import Image
-from torch.utils.data import Dataset
 import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+
 
 class CarDamageDataset(Dataset):
-    """
-    Dataset cho segmentation:
-      - img_dir: thư mục chứa ảnh .jpg
-      - anno_dir: thư mục chứa mask .png (grayscale 0/1)
-      - split_file: file .txt liệt kê các tên file (không có đuôi)
-      - transform: torchvision.transforms áp cho cả ảnh và mask
-    """
-    def __init__(self, img_dir, anno_dir, split_file, transform=None):
-        self.img_dir   = img_dir
-        self.anno_dir  = anno_dir
-        self.transform = transform
+    def __init__(self, img_dir, mask_dir, names_list,
+                 img_transform=None, mask_transform=None):
+        """
+        img_dir:    folder chứa ảnh (.jpg/.jpeg)
+        mask_dir:   folder chứa mask (.png)
+        names_list: list tên file (không đuôi), vd ["0001", ...]
+        img_transform:  torchvision.transforms áp dụng lên ảnh
+        mask_transform: torchvision.transforms áp dụng lên mask
+        """
+        self.img_dir        = img_dir
+        self.mask_dir       = mask_dir
+        self.img_transform  = img_transform
+        self.mask_transform = mask_transform
 
-        # Đọc danh sách tên sample
-        with open(split_file, 'r') as f:
-            self.names = [line.strip() for line in f if line.strip()]
 
-        # binary segmentation => 1 foreground + 1 background
-        self.num_classes = 2
+        # Lọc những sample có cả ảnh và mask
+        valid = []
+        for name in names_list:
+            # check ảnh
+            found_img = any(os.path.exists(os.path.join(img_dir, name + ext))
+                            for ext in (".jpg", ".jpeg"))
+            # check mask
+            found_mask = os.path.exists(os.path.join(mask_dir, name + ".png"))
+            if found_img and found_mask:
+                valid.append(name)
+        self.names = valid
+        print(f"[Dataset] {len(self.names)}/{len(names_list)} valid samples")
+
 
     def __len__(self):
         return len(self.names)
 
+
     def __getitem__(self, idx):
         name = self.names[idx]
-        img_path  = os.path.join(self.img_dir,  name + '.jpg')
-        mask_path = os.path.join(self.anno_dir, name + '.png')
 
-        img  = Image.open(img_path).convert('RGB')
-        mask = Image.open(mask_path).convert('L')  # 0/255
 
-        if self.transform:
-            img  = self.transform(img)
-            # resize mask, then convert to long tensor of 0/1
-            mask = self.transform(mask)
-            mask = (mask > 0).long()
-        else:
-            mask = torch.from_numpy(np.array(mask) > 0).long()
+        # 1) Load image
+        for ext in (".jpg", ".jpeg"):
+            img_path = os.path.join(self.img_dir, name + ext)
+            if os.path.exists(img_path):
+                image = Image.open(img_path).convert("RGB")
+                break
 
-        # mask shape [1,H,W]
-        return img, mask
+
+        # 2) Load mask
+        mask_path = os.path.join(self.mask_dir, name + ".png")
+        mask      = Image.open(mask_path).convert("L")  # grayscale
+
+
+        # 3) Apply transforms
+        if self.img_transform:
+            image = self.img_transform(image)
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+            # mask tensor will be float in [0,1], convert to binary 0/1
+            mask = (mask > 0).float()
+
+
+        return image, mask
